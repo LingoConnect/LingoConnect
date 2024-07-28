de0cfc6c65d10e79177a59bd1e470899082ce947
yezzero <yeyeonggim06@gmail.com>
2024-07-28 12:41:09 +0900

    Chore: 레이아 깨짐 수정

--- LingoConnect_frontend/src/test_pages/feedback_result_test.jsx
+++ LingoConnect_frontend/src/test_pages/feedback_result_test.jsx
@@ -7,6 +7,7 @@ import { AIChat, UserChat, AIFeedback, ScoreBox } from './practice_test';
 
 export default function FeedbackResultTest() {
     const [answers, setAnswers] = useState(['수학이 제일 좋아']);
+    const [index, setIndex] = useState(1);
     const { topic, question } = useParams();
     const navigate = useNavigate();
 
@@ -16,7 +17,7 @@ export default function FeedbackResultTest() {
             <img
                 className="feedbackresult-back"
                 src={process.env.PUBLIC_URL + '/img/arrow.png'}
-                onClick={()=>navigate('/mypage/feedback')}/>
+                onClick={()=>navigate(`/mypage/feedback/${topic}`)}/>
             <div className="feedbackresult-navbar">
                 <SmallTitle />
             </div>
@@ -26,11 +27,42 @@ export default function FeedbackResultTest() {
                 <h4>Q. {question}?</h4>
             </div>
 
-            {/* <div className="feedbackresult-index">
-                여러 개의 대화를 페이지네이션으로 ?
-            </div> */}
+            <div className="feedbackresult-index">
+                <div className="feedbackresult-index-left">
+                    {
+                        (index > 1) &&
+                        <h4 onClick={(e)=>{
+                                if (index>0) {
+                                    setIndex(index-1);
+                                }
+                                e.stopPropagation();                
+                        }}>&lt;</h4>
+                    }
+                </div>
+                <div className="feedbackresult-index-middle">
+                    <p onClick={()=>{
+                        if (index>1){
+                            setIndex(index-1)
+                        }
+                    }}>{index}</p>
+                    <p>{index+1}</p>
+                    <p onClick={()=>setIndex(index+1)}>{index+2}</p>
+                </div>
+                <div className="feedbackresult-index-right">
+                    {
+                        (index != 10) &&
+                        <h4 onClick={(e)=>{
+                                if (index<100) {
+                                    setIndex(index+1);
+                                }
+                                e.stopPropagation();                
+                        }}>&gt;</h4>
+                    } 
+                </div>
+            </div>
 
             <div className="feedbackresult-box">
+                {/* <h5>{index+1}</h5> */}
                 <AIChat question={question} />
                 <UserChat index={0} answers={answers} />
                 <AIFeedback index={0} test_feedback={test_feedback} />
@@ -41,13 +73,13 @@ export default function FeedbackResultTest() {
                 <UserChat index={0} answers={answers} />
                 <AIFeedback index={0} test_feedback={test_feedback} />
             </div>
-
+{/* 
             <div className="result-box-gradient">
                 <p></p>
             </div>
             <div className="result-box-under">
                 <p></p>
-            </div>
+            </div> */}
         </div>
     )
 }
