a58f3b21aa4d636497f020fef2c397b70d38e7f2
YenaLey <yena.e121@gmail.com>
2024-07-29 17:16:26 +0900

    Feat: 배경 도형 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BigCircle, SmallCircle } from './components/circle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="background-circle-container">
+      <BigCircle />
+      <BigCircle />
+      <BigCircle />
+      <BigCircle />
+      <BigCircle />
+      <BigCircle />
+      <SmallCircle />
+      <SmallCircle />
+      <SmallCircle />
+      <SmallCircle />
+    </div>
   </React.StrictMode>
 );
 
