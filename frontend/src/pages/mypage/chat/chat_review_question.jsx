d3bb2b027b053c6b73313f3bfe636dd189443c84
YenaLey <yena.e121@gmail.com>
2024-08-01 14:03:26 +0900

    Feat: 피드백 모아보기 api 연결

--- frontend/src/pages/mypage/chat/chat_review_question.jsx
+++ frontend/src/pages/mypage/chat/chat_review_question.jsx
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../../../styles/chat_review_question.css';
+import { getMyFeedback } from '../../../api/mypage_api';
 import { getMainQuestion } from '../../../api/learning_content_api';
 
 export default function ReviewQuestion() {
   const navigate = useNavigate();
   const location = useLocation();
   const { topic } = location.state || {};
+  const [totalMainQuestions, setTotalMainQuestions] = useState([]);
   const [mainQuestions, setMainQuestions] = useState([]);
+  const [getFeedback, setGetFeedback] = useState([]);
 
   const handleQuestionClick = (element) => {
-    const question = element.question;
-    const id = element.mainQuestionId;
-    navigate('/mypage/chat-review/practice', { state: { topic, id, question } });
+    const filteredFeedback = getFeedback.filter(
+      (feedback) => feedback.topQuestionId === element.mainQuestionId
+    );
+    const selectedMainQuestion = element.question;
+    navigate('/mypage/chat-review/practice', { state: { filteredFeedback, selectedMainQuestion } });
   };
 
   useEffect(() => {
     const fetchMainQuestion = async () => {
-      const response = await getMainQuestion({ topic });
-      if (response.status === 200) {
-        setMainQuestions(response.data);
+      const total_response = await getMainQuestion({ topic });
+      if (total_response.status === 200) {
+        setTotalMainQuestions(total_response.data);
+      }
+
+      const feedback_response = await getMyFeedback({ topic });
+      if (feedback_response.status === 200) {
+        setGetFeedback(feedback_response.data);
       }
     };
+
     fetchMainQuestion();
   }, [topic]);
+
+  useEffect(() => {
+    const filteredMainQuestions = totalMainQuestions.filter((mainQuestion) =>
+      getFeedback.some((feedback) => feedback.topQuestionId === mainQuestion.mainQuestionId)
+    );
+    setMainQuestions(filteredMainQuestions);
+  }, [totalMainQuestions, getFeedback]);
 
   return (
     <div className="feedbackquestion-container">
