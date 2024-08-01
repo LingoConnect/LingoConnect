35637a97f2af9c0e72ab07da2f9e13e082449d1d
YenaLey <yena.e121@gmail.com>
2024-08-01 10:29:10 +0900

    Chore: 튜토리얼 파일 에러 수정

--- frontend/src/pages/tutorial/tutorial_chat_pattern_practice.jsx
+++ frontend/src/pages/tutorial/tutorial_chat_pattern_practice.jsx
@@ -9,12 +9,12 @@ export default function TutorialPatternPractice() {
     '질문에 대해 단답형으로 대답하는 경향이 있습니다. 대답을 할 때 두세 문장 이상으로 대답해보는 연습을 해보아요!';
   const [index, setIndex] = useState(0);
   const [patternTutorial] = useState([
-    '이 페이지에서는 주제별로 피드백들을 분석해서 자주하는 실수를 보여줘요.', 
-    '마찬가지로 마이페이지의 실수 분석 메뉴에서 볼 수 있답니다.', 
+    '이 페이지에서는 주제별로 피드백들을 분석해서 자주하는 실수를 보여줘요.',
+    '마찬가지로 마이페이지의 실수 분석 메뉴에서 볼 수 있답니다.',
     '앱 사용법 설명은 여기서 마칠게요!',
     '메인 화면에서 언제든지 다시 설명을 볼 수 있어요.',
-    '그럼 이제 메인 화면으로 돌아가서 즐거운 학습을 시작해봅시다!'
-  ])
+    '그럼 이제 메인 화면으로 돌아가서 즐거운 학습을 시작해봅시다!',
+  ]);
 
   return (
     <div className="PatternResult-container">
@@ -24,7 +24,7 @@ export default function TutorialPatternPractice() {
 
       <div className="PatternResult-main">
         <h4>자주 하는 실수(패턴) 분석</h4>
-        <p className={index < 2 ? 'tutorial-main':''}>학교</p>
+        <p className={index < 2 ? 'tutorial-main' : ''}>학교</p>
       </div>
 
       <div className="PatternResult-card" style={{ marginTop: '8vh' }}>
@@ -47,7 +47,7 @@ export default function TutorialPatternPractice() {
 function ResultCard({ index, pattern }) {
   return (
     <div className="resultcard-container">
-      <div className={index < 2 ? "resultcard-card tutorial-main" : "resultcard-card"}>
+      <div className={index < 2 ? 'resultcard-card tutorial-main' : 'resultcard-card'}>
         <img src={process.env.PUBLIC_URL + '/img/light.png'} alt="" />
         <h4>{pattern}</h4>
       </div>
@@ -55,18 +55,19 @@ function ResultCard({ index, pattern }) {
   );
 }
 
-function Modal({index, setIndex, patternTutorial}) {
+function Modal({ index, setIndex, patternTutorial }) {
   const navigate = useNavigate();
 
   return (
-      <div 
-        className="tutorialModal pattern-modal"
-        style={{bottom:'20px'}}
-      >
-          <img src={process.env.PUBLIC_URL + '/img/cat.png'} alt="튜토리얼" />
-          <h4>{patternTutorial[index]}</h4>
-          {index < 4 && <p onClick={()=> setIndex(index+1)}>다음 &gt;</p>}
-          {index === 4 && <p onClick={()=> navigate('/study/chat')} style={{color:'rgb(0,10,255)'}}>메인으로</p>}
-      </div>
-  )
+    <div className="tutorialModal pattern-modal" style={{ bottom: '20px' }}>
+      <img src={process.env.PUBLIC_URL + '/img/cat.png'} alt="튜토리얼" />
+      <h4>{patternTutorial[index]}</h4>
+      {index < 4 && <p onClick={() => setIndex(index + 1)}>다음 &gt;</p>}
+      {index === 4 && (
+        <p onClick={() => navigate('/study/chat')} style={{ color: 'rgb(0,10,255)' }}>
+          메인으로
+        </p>
+      )}
+    </div>
+  );
 }
